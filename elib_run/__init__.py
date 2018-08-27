54f815381dc511f2f51e59cbe7510ac009f1ae82
etcher <github@daribouca.net>
2018-08-27 16:23:54 +0200

    expose function outside of the package

--- elib_run/__init__.py
+++ elib_run/__init__.py
@@ -5,6 +5,14 @@ Top-level package for elib_run.
 
 from pkg_resources import DistributionNotFound, get_distribution
 
+from ._exc import ELIBRunError, ExecutableNotFoundError
+from ._find_exe import find_executable
+from ._output import (
+    register_hook_cmd_end, register_hook_cmd_start, register_hook_error, register_hook_info,
+    register_hook_std_err, register_hook_std_out,
+)
+from ._run import run
+
 try:
     __version__ = get_distribution('elib_run').version
 except DistributionNotFound:  # pragma: no cover
